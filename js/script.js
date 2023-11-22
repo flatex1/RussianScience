297df5a4238af623d7d8a65a14a41b285cca40e5
flatex <flatex@duck.com>
2023-11-22 04:12:21 +0300

    add preloader

--- js/script.js
+++ js/script.js
@@ -268,6 +268,12 @@ window.onload = function () {
         yPercent: 120
     });
 
+    document.body.classList.add('loaded_hiding');
+    window.setTimeout(function () {
+      document.body.classList.add('loaded');
+      document.body.classList.remove('loaded_hiding');
+    }, 500);
+
 };
 
 
